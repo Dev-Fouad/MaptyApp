3f88b945197cd1e8fbba2bd3c30f18aef8a0e19f
fouad ajayi <fouadajayi6@gmail.com>
2022-11-26 18:26:51 +0100

    dfghj

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ class Workout {
     _setDescription () {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-        this.description = `${this.type[0].toUppercase()}${this.type.slice(1)} on ${this.date.getMonth()}`
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${ months[this.date.getMonth()]} ${this.date.getDate()}`
     }
     
 }
@@ -35,6 +35,7 @@ class Running extends Workout {
         super(coords, distance, duration)
         this.Cadence  = Cadence
         this.calcPace()
+        this.description()
     }
 
     calcPace(){
@@ -49,6 +50,7 @@ class Cycling extends Workout {
         super(coords, distance, duration)
         this.elevationGain  = elevationGain
         this.calcSpeed()
+        this.description()
     }
 
     calcSpeed() {
@@ -98,6 +100,14 @@ class App {
         this.#mapEvent = mapE
         form.classList.remove('hidden')
         inputDistance.focus()
+    }
+
+    _hideForm(){
+        // Empty inputs
+        inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value
+        form.style.display = 'none'
+        form.classList.add('hidden')
+        setTimeout(() => form.style.display = 'grid', 1000)
     }
 
     _toggleElevationField() {
@@ -155,7 +165,7 @@ class App {
         this._renderWorkout(workout)
 
         // Hide form + clear input fields
-        inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value
+        this._hideForm()
     }
 
 
@@ -171,14 +181,14 @@ class App {
               className: `${workout.type}-popup`
             })
         )
-        .setPopupContent('Workout')
+        .setPopupContent(`${workout.type === 'running' ? '🏃' : '🚴‍♀️'}${workout.description}`)
         .openPopup();
     }
 
     _renderWorkout(workout){
         let html = `
-            <li class="workout workout--running" data-id="1234567890">
-               <h2 class="workout__title">Running on April 14</h2>
+            <li class="workout workout--${workout.type}" data-id="${workout.id}">
+               <h2 class="workout__title">${workout.description}</h2>
                 <div class="workout__details">
                   <span class="workout__icon">${workout.type === 'running' ? '🏃' : '🚴‍♀️'}</span>
                   <span class="workout__value">${workout.duration}</span>
@@ -188,10 +198,39 @@ class App {
                   <span class="workout__icon">⏱</span>
                   <span class="workout__value">${workout.duration}</span>
                   <span class="workout__unit">min</span>
-                </div>
-        `
+                </div>    
+        `;
+
+        if (workout.type === 'running')
+          html += `
+            <div class="workout__details">
+                <span class="workout__icon">⚡️</span> 
+                <span class="workout__value">${workout.pace.toFixed(1)}</span>
+                <span class="workout__unit">min/km</span>
+            </div>
+            <div class="workout__details">
+                <span class="workout__icon">🦶🏼</span>
+                <span class="workout__value">${workout.cadence}</span>
+                <span class="workout__unit">spm</span>
+            </div>
+           </li>
+        `;
+
+        if (workout.type === 'cycling')
+          html += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed[1]}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        `;
+
+        form.insertAdjacentHTML('afterend' , html)
     }
 }
-
-let app = new App()
- 
+let app = new App();
