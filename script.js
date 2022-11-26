09c60ba4327b720fa3a7be9e193913f84c42524f
fouad ajayi <fouadajayi6@gmail.com>
2022-11-26 12:53:25 +0100

    dfghj

--- script.js
+++ script.js
@@ -11,6 +11,41 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+    date = new Date()
+    id = (new Date() + '').slice(-10)
+
+    constructor (coords, distance, duration) {
+        this.coords = coords
+        this.distance = distance
+        this.duration = duration
+    }
+
+}
+
+class Running extends Workout {
+    constructor(coords , distance, duration, Cadence){
+        super(coords, distance, duration)
+        this.Cadence  = Cadence
+        this.calcPace()
+    }
+
+    calcPace(){
+        this.pace = this.duration / this.distance
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords , distance, duration, elevationGain){
+        super(coords, distance, duration)
+        this.elevationGain  = elevationGain
+        this.calcSpeed()
+    }
+
+    calcSpeed() {
+        this.speed = this.distance / (this.duration / 60)
+    }
+}
 
 class App {
     #map;
