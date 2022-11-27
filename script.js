596465761506766bab3845f78421d7a29d5d15ec
fouad ajayi <fouadajayi6@gmail.com>
2022-11-27 23:51:41 +0100

    Functionalties

--- script.js
+++ script.js
@@ -7,20 +7,22 @@ class Workout {
     clicks = 0;
 
   constructor(coords, distance, duration) {
-    this.coords = coords;
-    this.distance = distance;
-    this.duration = duration;
-  }
+     this.coords = coords;
+     this.distance = distance;
+     this.duration = duration;
+    }
 
-  _setDescription() {
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    _setDescription() {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;}
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
 
-  click() {
-    this.clicks++;
-  }
+    click() {
+        this.clicks++;
+    }
 }
+
 
 class Running extends Workout {
   type = 'running';
@@ -39,7 +41,7 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
-  type = 'cycling';
+    type = 'cycling';
 
   constructor(coords, distance, duration, elevationGain) {
         super(coords, distance, duration);
@@ -47,10 +49,9 @@ class Cycling extends Workout {
         // this.type = 'cycling';
         this.calcSpeed();
         this._setDescription();
-  }
+    }
 
   calcSpeed() {
-    // km/h
     this.speed = this.distance / (this.duration / 60);
     return this.speed;
   }
@@ -69,65 +70,62 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
-  #map;
-  #mapZoomLevel = 13;
-  #mapEvent;
-  #workouts = [];
+     #map;
+     #mapZoomLevel = 13;
+     #mapEvent;
+     #workouts = [];
 
   constructor() {
-    // Get user's position
-    this._getPosition();
+     // Get user's position
+     this._getPosition();
 
-    // Get data from local storage
-    this._getLocalStorage();
+     // Get data from local storage
+     this._getLocalStorage();
 
-    // Attach event handlers
-    form.addEventListener('submit', this._newWorkout.bind(this));
-    inputType.addEventListener('change', this._toggleElevationField);
-    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
-  }
+     // Attach event handlers
+     form.addEventListener('submit', this._newWorkout.bind(this));
+     inputType.addEventListener('change', this._toggleElevationField);
+     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
+    }
 
   _getPosition() {
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
-         alert('Could not get your position')
-        })
+          alert('Could not get your position')
+      })
   }
 
   _loadMap(position) {
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+         const { latitude } = position.coords;
+         const { longitude } = position.coords;
 
-    const coords = [latitude, longitude];
+         const coords = [latitude, longitude];
 
-    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+         this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.#map);
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', { attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(this.#map);
 
-    // Handling clicks on map
-    this.#map.on('click', this._showForm.bind(this));
+        // Handling clicks on map
+        this.#map.on('click', this._showForm.bind(this));
 
-    this.#workouts.forEach(work => {
-      this._renderWorkoutMarker(work);
-    });
+        this.#workouts.forEach(work => {
+             this._renderWorkoutMarker(work);
+        });
   }
 
   _showForm(mapE) {
-    this.#mapEvent = mapE;
-    form.classList.remove('hidden');
-    inputDistance.focus();
+        this.#mapEvent = mapE;
+         form.classList.remove('hidden');
+         inputDistance.focus();
   }
 
   _hideForm() {
-    // Empty inputs
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-    form.style.display = 'none';
-    form.classList.add('hidden');
-    setTimeout(() => (form.style.display = 'grid'), 1000);
+     inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+     form.style.display = 'none';
+     form.classList.add('hidden');
+     setTimeout(() => (form.style.display = 'grid'), 1000);
   }
 
   _toggleElevationField() {
@@ -136,18 +134,17 @@ class App {
   }
 
   _newWorkout(e) {
-    const validInputs = (...inputs) =>
-      inputs.every(inp => Number.isFinite(inp));
-    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+     const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+     const allPositive = (...inputs) => inputs.every(inp => inp > 0);
 
     e.preventDefault();
 
     // Get data from form
-    const type = inputType.value;
-    const distance = +inputDistance.value;
-    const duration = +inputDuration.value;
-    const { lat, lng } = this.#mapEvent.latlng;
-    let workout;
+     const type = inputType.value;
+     const distance = +inputDistance.value;
+     const duration = +inputDuration.value;
+     const { lat, lng } = this.#mapEvent.latlng;
+     let workout;
 
     // If workout running, create running object
     if (type === 'running') {
